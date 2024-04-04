f4322b023c2ee4947d5044a4c801247dcda8472f
nalle631 <malteolofsson97@gmail.com>
2024-04-04 12:07:12 +0200

    time

--- chaincode/b2b/bumpy-contract/chaincode/smartcontract.go
+++ chaincode/b2b/bumpy-contract/chaincode/smartcontract.go
@@ -80,7 +80,7 @@ func (s *SmartContract) Create(ctx contractapi.TransactionContextInterface, tech
 		return nil, fmt.Errorf("Job %s does not exist off ledger", jobID)
 	}
 
-	timeDeadline, err := time.Parse("2006-01-02T15:04:05Z07:00", deadline)
+	timeDeadline, err := time.Parse("0001-01-01 00:00:00 +0000 UTC", deadline)
 	if err != nil {
 		fmt.Println("Error parsing deadline: ", err)
 		return nil, err
