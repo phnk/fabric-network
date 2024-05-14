b9ef1efb7742996e063341f5635ae04fb365237e
nalle631 <malteolofsson97@gmail.com>
2024-05-14 15:29:06 +0200

    added jespers address

--- chaincode/b2b/job-contract/chaincode/smartcontract.go
+++ chaincode/b2b/job-contract/chaincode/smartcontract.go
@@ -158,7 +158,7 @@ func (s *SmartContract) TakeJob(ctx contractapi.TransactionContextInterface, job
 	}
 
 	//Add jespers VM address to get request
-	serviceLevelResponse, err := http.Get("local/" + jobInfo.ProductID + "/servicelevel")
+	serviceLevelResponse, err := http.Get("http://35.228.100.1:80/" + jobInfo.ProductID + "/service-level")
 	if err != nil {
 		return err
 	}
